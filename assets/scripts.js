68701c99c460b182872c610f4012ee492cb87363
Michael Grienauer <mgrienauer@gmail.com>
2018-03-28 17:36:46 +0200

    updated streamer div layouts when active
    updated jquery code to change styling when a streamer is active .

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,17 +13,17 @@ $(document).ready(function(){
        'Client-ID': 'ieym1jzi0ljgp9ntvvw0dcuxaw4hzf'
      },
      success: function(data) {
-       console.log(data);
+       //console.log(data);
        //create variable to store streamer div
        let streamerDiv = `
-        <div class="streamer-div streamer-offline">
-          <i class="fa fa-twitch fa-2x icon-offline"></i>
+        <div class="streamer-div streamer-offline" id=${streamer}>
+          <i class="fa fa-twitch fa-2x icon-offline" id=${streamer}-icon></i>
           <img src=${data.logo} class='streamer-logo'></img>
-          <a href=${data._links.self} class='streamer-name'> <h3>${data.display_name}</h3> </a>
+          <a href=${data.url} target='#blank' class='streamer-name'> <h3>${data.display_name}</h3> </a>
           <h3 class='streamer-followers'> Followers: ${data.followers} <h3>
         </div>
        `;
-
+       //append the streamer's div to html doc
        $('.wrapper').append(streamerDiv);
      }
     });
@@ -43,20 +43,31 @@ $(document).ready(function(){
      },
      // log data to console if success
      success: function(data) {
-       //console.log(data);
-       //create function that takes twitch api data and updates html on page
-       function updateStreamData(){
-         //update html on streamer div to display current stream info
-         if (data.stream !== null){
-
-         }
+       console.log(data);
+       if (data.stream !== null){
+         updateStreamData();
        }
+
+      //create function that takes twitch api data and updates html on page
+      function updateStreamData(){
+        //create vars to store status data and viewers
+        let streamStatus = data.stream.channel.status;
+        let streamViewers = data.stream.viewers;
+        //remove streamer followers
+        $(`#${streamer} .streamer-followers`).remove();
+        //append stream status and current viewers
+        $('#'+streamer).append(`<p class='stream-status'>${streamStatus}</p>`);
+        $('#'+streamer).append(`<h3 class='stream-viewers'>Viewers:<br/>${streamViewers}`);
+        $(`#${streamer}`).toggleClass('streamer-offline streamer-online');
+        $(`#${streamer}-icon`).toggleClass('icon-offline icon-online');
+      }
      }
     });
   }
 
   //for each streamer, make an ajax request and create an html div with relevant info
   streamerArr.forEach(ajaxChannelCall);
+  streamerArr.forEach(ajaxStreamCall);
 
 
 
