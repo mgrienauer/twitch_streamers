6ccb8caee6e12d6499421948dc941fbbfcdc90db
Michael Grienauer <mgrienauer@gmail.com>
2018-03-28 15:57:22 +0200

    jquery code for streamer div info
    added jquery code to add a styling div with streamer's info. still need to add code to filter if user online

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,40 @@
 $(document).ready(function(){
 
+  //create array to store list of streamer usernames
+  let streamerArr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "T90Official", "theviperaoc", "RobotCaleb", "noobs2ninjas"];
+
+  //create function that updates streamer's channel info in html
+  function ajaxChannelCall(streamer){
+    //make ajax call to retrieve streamer's channel info
+    $.ajax({
+     type: 'GET',
+     url: 'https://api.twitch.tv/kraken/channels/' + streamer,
+     headers: {
+       'Client-ID': 'ieym1jzi0ljgp9ntvvw0dcuxaw4hzf'
+     },
+     success: function(data) {
+       console.log(data);
+       //create variable to store streamer div
+       let streamerDiv = `
+        <div class="streamer-div streamer-offline">
+          <i class="fa fa-twitch fa-2x icon-offline"></i>
+          <img src=${data.logo} class='streamer-logo'></img>
+          <a href=${data._links.self} class='streamer-name'> <h3>${data.display_name}</h3> </a>
+          <h3 class='streamer-followers'> Followers: ${data.followers} <h3>
+        </div>
+       `;
+
+       $('.wrapper').append(streamerDiv);
+     }
+    });
+
+
+  }
+
+
   //create function that take a streamer username as input and returns json data from twitch api
-  function ajaxCall(streamer){
-    //make ajax call with streamer name
+  function ajaxStreamCall(streamer){
+    //make ajax call with streamer name to check stream status
     $.ajax({
      type: 'GET',
      url: 'https://api.twitch.tv/kraken/streams/' + streamer,
@@ -11,16 +43,20 @@ $(document).ready(function(){
      },
      // log data to console if success
      success: function(data) {
-       console.log(data);
+       //console.log(data);
+       //create function that takes twitch api data and updates html on page
+       function updateStreamData(){
+         //update html on streamer div to display current stream info
+         if (data.stream !== null){
+
+         }
+       }
      }
     });
   }
 
-  //create array to store list of streamer usernames
-  let streamerArr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "theviperaoc", "RobotCaleb", "noobs2ninjas"];
-
   //for each streamer, make an ajax request and create an html div with relevant info
-  streamerArr.forEach(ajaxCall);
+  streamerArr.forEach(ajaxChannelCall);
 
 
 
